1b0e030271f841291059c7cde78ed7b17c8f85ff
tecita <hectorin_cintia@hotmail.com>
2019-12-05 20:34:18 -0600

    proyecto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Homeee</a>
+                        <a href="{{ url('/home') }}">Homeeeee</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
